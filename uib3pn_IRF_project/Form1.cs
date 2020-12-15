422c9db5fc2f27341f454a9b2f5cc6c877cba293
Ádám <adam.purman@stud.uni-corvinus.hu>
2020-12-15 20:07:37 +0100

    initial commit

---
+++ uib3pn_IRF_project/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uib3pn_IRF_project.Entities;
+
+namespace uib3pn_IRF_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+    }
+}
