575ffcfaba96a6d15e11f4cb8890624ade474208
Ádám <adam.purman@stud.uni-corvinus.hu>
2020-12-15 20:16:42 +0100

    first commit

---
+++ uib3pn_IRF_project/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uib3pn_IRF_project.Entities
+{
+    class Employee
+    {
+        string FirstName { get; set; }
+        string Email { get; set; }
+        CountryEnum Country { get; set; }
+    }
+}
--- uib3pn_IRF_project/Form1.cs
+++ uib3pn_IRF_project/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using uib3pn_IRF_project.Entities;
 
 namespace uib3pn_IRF_project
@@ -16,7 +19,12 @@ namespace uib3pn_IRF_project
         public Form1()
         {
             InitializeComponent();
-            
+
+            var xml = new XmlDocument();
+            //xml.LoadXml(GeneratedData);
+            String path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"GeneratedData.xml");
+            xml.LoadXml(path);
+
         }
     }
 }
